3f535d6d8172acf1760da22b89983318f91aecad
Antvictor <larryyangc@163.com>
2023-03-23 10:43:12 +0800

    fix: 修复algolia不生效问题

--- src/.vuepress/plugin.ts
+++ src/.vuepress/plugin.ts
@@ -13,11 +13,11 @@ docsearchPlugin({
     // docSearch的AppId
     appId:"8MU38TRSDV",
     // 搜索类型
-    searchParameters:"query",
+    // searchParameters:"query",
     // 搜索框默认字段
     placeholder:"搜索文章",
     // 打开弹窗时的初始请求
-    initialQuery:"",
+    // initialQuery:"",
     translations:{button: {
         buttonText: '搜索文章',
       },
